84f183a56b2abdeef93447ab4c3d892ca1968363
TaylorTheDeveloper <TaylorTheDeveloper@gmail.com>
2016-04-11 01:35:10 -0400

    working on bfs

--- bipartite.py
+++ bipartite.py
@@ -1,11 +1,13 @@
-
+from collections import deque
 
 
 
 def checkBipartite(graph,root=0):
 	#Returns true if a graph is bipartite
 	#Defaults to start on root 0, but can be started on any node
-	
+	colors = list()
+	queue = deque()
+
 	if not graph:
 		print "Error: Null Graph"
 		return False
@@ -13,6 +15,24 @@ def checkBipartite(graph,root=0):
 	if root > len(graph[0]) or root < 0:
 		print "Error: Select Valid Root"
 		return False
+
+	#The Color Array will store the colors of each vertex 
+	#True = Purple
+	#False = Green
+	#None = Unpainted
+	for i in range(0,len(graph)):
+		colors.append(None)
+
+	colors[root] = True
+	queue.append(root)
+
+	#Perform Breadth First Search and mark nodes of the graph the proper color
+	while len(queue) != 0:
+		node = queue.popleft()
+
+		
+		
+
 
 
 
