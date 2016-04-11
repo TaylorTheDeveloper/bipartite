40ba26a69ca0499cd39a5e4f61ede698be304d78
TaylorTheDeveloper <TaylorTheDeveloper@gmail.com>
2016-04-11 01:16:35 -0400

    added some errok handling

--- bipartite.py
+++ bipartite.py
@@ -1,6 +1,22 @@
 
 
 
+
+def checkBipartite(graph,root=0):
+	#Returns true if a graph is bipartite
+	#Defaults to start on root 0, but can be started on any node
+	
+	if not graph:
+		print "Error: Null Graph"
+		return False
+
+	if root > len(graph[0]) or root < 0:
+		print "Error: Select Valid Root"
+		return False
+
+
+
+
 if __name__ == "__main__":
 
 	#Matrix Representation of graph
@@ -8,5 +24,9 @@ if __name__ == "__main__":
 			 [1,1,1],\
 			 [1,1,1]]
 
+	#graph = []
 
-	print graph[0][0]
+	if checkBipartite(graph):
+		print "This Graph is bipartite"
+	else:
+		print "This Graph is not bipartite"
